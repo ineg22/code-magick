0949e046795bfd5c2163390082c2acd38e104603
ineg22 <ineg22@gmail.com>
2019-05-24 15:52:56 +0300

    add eventListeners

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,49 @@
 'use strict';
 
-document.querySelector('.setup').classList.remove('hidden');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-var similarListElement = document.querySelector('.setup-similar-list');
-
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 var WIZARD_NAMES = ['Иван','Хуан Себастьян','Мария','Кристоф','Виктор','Юлия','Люпита','Вашингтон'];
 var WIZARD_FEMALES = ['да Марья','Верон','Мирабелла','Вальц','Онопко','Топольницкая','Нионго','Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)','rgb(241, 43, 107)','rgb(146, 100, 161)','rgb(56, 159, 117)','rgb(215, 210, 55)','rgb(0, 0, 0)'];
 var EYES_COLORS = ['black','red','blue','yellow','green'];
 
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var similarListElement = document.querySelector('.setup-similar-list');
+var setupOpenElement = document.querySelector('.setup-open');
+var setupCloseElement = document.querySelector('.setup-close');
+var setupElement = document.querySelector('.setup');
+
+setupOpenElement.addEventListener('click', openPopup);
+setupOpenElement.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup;
+  }
+});
+
+setupCloseElement.addEventListener('click', closePopup);
+setupCloseElement.addEventListener('keydown', function (evt) {
+  if (evt.keyCode == ENTER_KEYCODE) {
+    closePopup;
+  }
+});
+
+function onEscPress(evt) {
+  if (evt.keyCode == ESC_KEYCODE) {
+    closePopup;
+  }
+}
+
+function closePopup() {
+  setupElement.classList.add('hidden');
+  document.removeEventListener('keydown', onEscPress)
+}
+
+function openPopup() {
+  setupElement.classList.remove('hidden');
+  document.addEventListener('keydown', onEscPress);
+}
+
+//--------------------------------------------------------------------
 var wizards = [];
 for (var i = 0; i < 4; i++) {
     wizards.push(generateWizard())
